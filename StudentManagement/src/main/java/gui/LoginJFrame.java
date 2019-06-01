164db49e2e49c473feff8bc602676f58a3bc1127
Tran T. Ngoc <ttngoc653@gmail.com>
2019-06-01 16:00:04 +0700

    make frame in center desktop

--- StudentManagement/src/main/java/gui/LoginJFrame.java
+++ StudentManagement/src/main/java/gui/LoginJFrame.java
@@ -19,6 +19,10 @@ public class LoginJFrame extends javax.swing.JFrame {
      */
     public LoginJFrame() {
         initComponents();
+        
+        java.awt.Dimension dim =java.awt. Toolkit.getDefaultToolkit().getScreenSize();
+        this.setLocation(dim.width / 2 - this.getSize().width / 2, dim.height / 2 - this.getSize().height / 2);
+        
     }
 
     /**
