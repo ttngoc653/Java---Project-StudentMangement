74b53490774a5854f672213287c3b31814bbdb94
tuhuynhkhtn <tuhuynhkhtn@gmail.com>
2019-06-18 18:15:13 +0700

    update login

--- StudentManagement/src/main/java/gui/LoginJFrame.java
+++ StudentManagement/src/main/java/gui/LoginJFrame.java
@@ -39,8 +39,6 @@ public class LoginJFrame extends javax.swing.JFrame {
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Đăng nhập");
 
-        txtTenTaiKhoan.setText("admin");
-
         btnDangNhap.setLabel("Đăng nhập");
         btnDangNhap.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
